24e3fb32b10c5dbf82717d5bade19af224787eb8
reubenkoshy <reuben.koshy@my.jcu.edu.au>
2018-08-23 11:37:49 +1000

    Added Prac 1 Files

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,15 +8,27 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celcius_to_fahrenheit():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celcius():
+    global celsius
+    celsius = 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        celcius_to_fahrenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         # TODO: Write this section to convert F to C and display the result
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        fahrenheit_to_celcius()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
