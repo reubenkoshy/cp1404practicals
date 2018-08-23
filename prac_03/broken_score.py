2e1ebb9d50de80dd9f27392e68f1adf2ac0f4f21
reubenkoshy <reuben.koshy@my.jcu.edu.au>
2018-08-23 11:43:54 +1000

    Added Functions to broken score program

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -6,12 +6,18 @@ Broken program to determine score status
 # TODO: Fix this!
 
 score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif 90 > score:
-        print("Passable")
-else:
-    print("Excellent")
 
+
+def check_score(value):
+    if value < 0 or value > 100:
+        message = "Invalid score"
+    elif value < 50:
+        message = "Bad"
+    elif 90 > value:
+        message = "Passable"
+    else:
+        message = "Excellent"
+    return message
+
+
+print(check_score(score))
