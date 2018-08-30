503fb1a31cbee2482a35c2a322fdd1a353a04bb7
reubenkoshy <reuben.koshy@my.jcu.edu.au>
2018-08-30 14:51:09 +1000

    added quick_picks program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,27 @@
+import random
+
+
+def main():
+
+    numbers_per_line = 6
+    minimum = 1
+    maximum = 45
+
+    number_picks = int(input("How many quick picks? "))
+    while number_picks < 0:
+        print("Invalid Number of Quick Picks!")
+        number_picks = int(input("How many quick picks? "))
+
+    for i in range(number_picks):
+        quick_pick = []
+        for j in range(numbers_per_line):
+            number = random.randint(minimum, maximum)
+            while number in quick_pick:
+                number = random.randint(minimum, maximum)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
