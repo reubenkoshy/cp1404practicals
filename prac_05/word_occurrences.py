cfdad9ae30285781b0487faab86a2c7b97db4059
reubenkoshy <reuben.koshy@my.jcu.edu.au>
2018-09-06 13:30:36 +1000

    fixed prac_05

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,17 +3,13 @@ each_word = {}
 text = input("Text: ")
 words = text.split()
 for word in words:
-    frequency = each_word.get(word, 0)
-    # Note: this is the "Look Before You Leap" (LBYL) pattern
-    # we could use the "Easier to Ask Forgiveness" pattern using exceptions
-    each_word[word] = frequency + 1
+    occurrences = each_word.get(word, 0)
 
-# Print the unique words and their frequencies,
-# in alphabetical order
+    each_word[word] = occurrences + 1
+
 words = list(each_word.keys())
 words.sort()
-# use the max function to find the maximum of the values produced by the
-# generator function (like a list comprehension) of lengths of words
+
 max_length = max((len(word) for word in words))
 for word in words:
-print("{:{}} : {}".format(word, max_length, each_word[word]))
+    print("{:{}} : {}".format(word, max_length, each_word[word]))
